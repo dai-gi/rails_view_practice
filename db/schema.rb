13e2949158aaf67d692a83687d92866c6188cc28
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-05-19 22:31:15 +0900

    Add: Bookモデルのスキーマを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_130314) do
+ActiveRecord::Schema.define(version: 2022_05_18_150129) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "status"
     t.datetime "finished_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
