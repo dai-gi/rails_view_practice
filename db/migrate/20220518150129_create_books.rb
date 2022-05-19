3226a59fc03f75d6eb0abf5968c520b3e6f31407
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-05-19 22:29:28 +0900

    Improve: 修正

--- db/migrate/20220518150129_create_books.rb
+++ db/migrate/20220518150129_create_books.rb
@@ -2,7 +2,6 @@ class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :title
       t.text :description
 
       t.timestamps
