0223d950610754703fae0f2358982fd5a2245fe1
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-05-22 23:57:00 +0900

    Check: scopeメソッドを使って自作のメソッドをカプセル化

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
-  validates :title, presence: true
+  # TODAY = Time.now.midnight
+  # scope :today_search, -> {where(created_at: TODAY..TODAY.tomorrow)}
+
+  # 引数を利用する
+  scope :title_search, ->(title) {where(title: title)}
+
 end
