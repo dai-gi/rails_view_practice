527f05682e5b917eda54bcaa3b9047398a3b496d
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-05-23 22:57:53 +0900

    Add: uniquenessを実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,13 +8,16 @@ class Book < ApplicationRecord
   # バリデーションヘルパー: format
   validates :title, :description, presence: true
   validates :title, format: { with: /\A[a-zA-Z]+\z/, message: "は英文字で記述してください"}
+
   # length
   validates :title, length: { in: 8..12,
     too_long: "タイトルは最大12桁です",
     too_short: "タイトルは最小8桁です"
   }
+
   # numericality
   validates :price, numericality: { only_integer: true }
   validates :number, numericality: { only_integer: true }
 
+  validates :number, uniqueness: true
 end
