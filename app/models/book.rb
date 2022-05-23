28842cf98a807a9e3f9612844a561a63e64b25fc
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-05-23 23:26:16 +0900

    Add: バリデーションヘルパーの共通オプション「on:」を実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,19 +5,23 @@ class Book < ApplicationRecord
   # 引数を利用する
   # scope :title_search, ->(title) {where(title: title)}
 
+  validates :title, presence: true
+  validates :description, presence: true, on: :update
+
   # バリデーションヘルパー: format
-  validates :title, :description, presence: true
-  validates :title, format: { with: /\A[a-zA-Z]+\z/, message: "は英文字で記述してください"}
+  # validates :title, format: { with: /\A[a-zA-Z]+\z/, message: "は英文字で記述してください"}
 
   # length
-  validates :title, length: { in: 8..12,
-    too_long: "タイトルは最大12桁です",
-    too_short: "タイトルは最小8桁です"
-  }
+  # validates :title, length: { in: 8..12,
+  #   too_long: "タイトルは最大12桁です",
+  #   too_short: "タイトルは最小8桁です"
+  # }
 
   # numericality
-  validates :price, numericality: { only_integer: true }
-  validates :number, numericality: { only_integer: true }
+  # validates :price, numericality: { only_integer: true }
+  # validates :number, numericality: { only_integer: true }
 
-  validates :number, uniqueness: true
+  # uniqueness
+  # validates :number, uniqueness: true
+
 end
