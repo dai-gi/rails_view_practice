d903ab71592c12be2e5b1cb7b462e13ff304de25
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-05-23 21:41:13 +0900

    Add: バリデーションヘルパーのformatを実装してみた

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,10 @@ class Book < ApplicationRecord
   # scope :today_search, -> {where(created_at: TODAY..TODAY.tomorrow)}
 
   # 引数を利用する
-  scope :title_search, ->(title) {where(title: title)}
+  # scope :title_search, ->(title) {where(title: title)}
+
+  # バリデーションヘルパー: format
+  validates :title, :description, presence: true
+  validates :title, format: { with: /\A[a-zA-Z]+\z/, message: "は英文字で記述してください"}
 
 end
